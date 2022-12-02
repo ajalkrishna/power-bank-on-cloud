4d33f1728ed4171b56d48d94d35d70677148a307
ajalkrishna <ajalkrishnav@gmail.com>
2022-12-02 19:17:52 +0530

    utility module configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'utility', loadChildren: () => import('./utility/utility.module').then(m => m.UtilityModule) },
+  {path:'home',component:HomeComponent},
+  {path:'login',component:LoginComponent},
+  { path: 'consumer', loadChildren: () => import('./consumer/consumer.module').then(m => m.ConsumerModule) },
+  { path: 'generator', loadChildren: () => import('./generator/generator.module').then(m => m.GeneratorModule) },
+  { path: 'retailer', loadChildren: () => import('./retailer/retailer.module').then(m => m.RetailerModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/generator/generator.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GeneratorRoutingModule } from './generator-routing.module';
+import { GeneratorComponent } from './generator.component';
+
+
+@NgModule({
+  declarations: [
+    GeneratorComponent
+  ],
+  imports: [
+    CommonModule,
+    GeneratorRoutingModule
+  ]
+})
+export class GeneratorModule { }
