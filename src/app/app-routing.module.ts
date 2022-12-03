d16e9123650eb10c9161acfd01fc5b2d861a508b
ajalkrishna <ajalkrishnav@gmail.com>
2022-12-03 09:46:04 +0530

    ui updation after merging

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { TradingComponent } from './trading/trading.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
   { path: 'utility', loadChildren: () => import('./utility/utility.module').then(m => m.UtilityModule) },
-  {path:'home',component:HomeComponent},
+  {path:'home',component:WelcomeComponent},
   {path:'login',component:LoginComponent},
   { path: 'consumer', loadChildren: () => import('./consumer/consumer.module').then(m => m.ConsumerModule) },
   { path: 'generator', loadChildren: () => import('./generator/generator.module').then(m => m.GeneratorModule) },
-  { path: 'retailer', loadChildren: () => import('./retailer/retailer.module').then(m => m.RetailerModule) }
+  { path: 'retailer', loadChildren: () => import('./retailer/retailer.module').then(m => m.RetailerModule) },
+  {path:'trading',component:TradingComponent},
+  {path:'',redirectTo:'home',pathMatch:'full'},
 ];
 
 @NgModule({
--- src/app/generator/generator.module.ts
+++ src/app/generator/generator.module.ts
@@ -1,17 +1,39 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms'
 
 import { GeneratorRoutingModule } from './generator-routing.module';
 import { GeneratorComponent } from './generator.component';
+import { PpaTableComponent } from './ppa-table/ppa-table.component';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PpaFormComponent } from './ppa-form/ppa-form.component';
+import { PbFormComponent } from './pb-form/pb-form.component';
+import { CreatePbComponent } from './create-pb/create-pb.component';
+import { CreatePpaComponent } from './create-ppa/create-ppa.component';
+import { MyGeneratorsComponent } from './my-generators/my-generators.component';
+import { UtilityDashboardComponent } from './utility-dashboard/utility-dashboard.component';
+
 
 
 @NgModule({
   declarations: [
-    GeneratorComponent
+    GeneratorComponent,
+    PpaTableComponent,
+    NavbarComponent,
+    PpaFormComponent,
+    PbFormComponent,
+    CreatePbComponent,
+    CreatePpaComponent,
+    MyGeneratorsComponent,
+    UtilityDashboardComponent
   ],
   imports: [
     CommonModule,
-    GeneratorRoutingModule
+    GeneratorRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    PaginationModule.forRoot()
   ]
 })
 export class GeneratorModule { }
